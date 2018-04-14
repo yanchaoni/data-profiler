19581a6c79daf47f3dc315f8dfe0e8cb6d487303
DylanFu <fudinglun94@berkeley.edu>
2018-04-14 15:32:45 -0400

    add comment

--- src/data_input.py
+++ src/data_input.py
@@ -1,18 +1,36 @@
 
 
+"""
+Transfer a list of input path in a list of dataframes
+Input:
+spark: the main spark session
+input_list: a list of input path
 
+Output:
+result: a list of data frame
+"""
 def handle_input(spark, input_list):
 	result = []
 	for path in input_list:
 		result.append(handle_single_input(spark, path))
+	return result
 
 
-def handle_single_input(sparkcontext, path):
+"""
+Handle single input path
+Input:
+spark: the main spark session
+path: the path to the input file
+
+Output:
+result: a table in form of data frame
+"""
+def handle_single_input(spark, path):
 	name, filetype = path.split('.')
 	if filetype == 'txt':
 		table = spark.read.text(path)
 	else:
-		####TO DO
+		####TO DO can hanld multiple input type
 		table = None
 	return table
 
--- src/model.py
+++ src/model.py
@@ -8,7 +8,16 @@ class DataProfiler():
 
 	def __init__(self, input_path_list):
 		"""
+		input:
 		input_path_list: a list of path of datasets
+
+
+		variable:
+		self.spark: main sparksession
+		self.input_list: input path list
+		self.num_of_table: number of tables in the core system
+		self.tables: reference to a list of tables as data frame in the core system
+
 		"""
 		self.spark =  SparkSession.builder.appName("DataProfiler").config("spark.some.config.option", "some-value").getOrCreate()
 		self.input_list = input_path_list
@@ -18,6 +27,8 @@ class DataProfiler():
 
 	def describe(self, indexes=None):
 		"""
+		Describe tables in the core system
+		input:
 		indexes: a list of index of table needed to be described
 		"""
 		if indexes == None:
