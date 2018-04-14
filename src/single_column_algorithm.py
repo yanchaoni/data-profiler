1526fa7eb948d3635602fee2e8300316502b77fb
yanchaoni <yn811@nyu.edu>
2018-04-13 21:53:53 -0400

    complete agg functions, to handle exceptions

--- src/single_column_algorithm.py
+++ src/single_column_algorithm.py
@@ -5,25 +5,38 @@ sc = SparkContext('local')
 spark = SparkSession(sc)
 
 def per_table_evaluate(tables, func_str, table_indexes, col_names, arg_str=None):
+    print(table_indexes, col_names)
+    print("table index: {0}".format(table_indexes))
+    print( "tables[{0}].".format(table_indexes))
     if isinstance(col_names, str):
-        col_names="'" + col_names + "'"
+        cols ="'" + col_names + "'"
     else:
-        col_names = "['" + "','".join(col_names) + "']"
+        cols = "['" + "','".join(col_names) + "']"
     if arg_str is None:
-        print(table_indexes, col_names)
-        print("table index: {0}".format(table_indexes))
-        print( "tables[{0}].".format(table_indexes))
-        func = "tables[{0}].".format(table_indexes) + func_str + "(" + col_names + ")"
+        func = "tables[{0}].".format(table_indexes) + func_str + "(" + cols + ")"
     else:
-        func = "tables[{0}].".format(table_indexes) + func_str + "(" + col_names + "," + rg_str + ")"
+        func = "tables[{0}].".format(table_indexes) + func_str + "(" + cols + "," + arg_str + ")"
     print(func)
-    return eval(func)
+    try:
+        result = eval(func)
+    except:
+        if isinstance(col_names, str):
+            dic =  "{'" + col_names + "':'" + func_str + "'}"
+        else:
+            dic =  "{'" + ("':'"+func_str+"','").join(col_names) + "':'" +  func_str + "'}"
+        func = "tables[{0}].".format(table_indexes) + "agg(" + dic + ")"
+        print(func)
+        result = eval(func)
+    return result
 
 def single_column_evaluate(tables, func_str, table_indexes, col_names, arg_str=None):
     profile=[]
     if isinstance(table_indexes, int): # 1 -> 1/many
         profile += [per_table_evaluate(tables, func_str, table_indexes, col_names, arg_str)]
     else: # many -> many
+# handle table index not list
+# handle function cannot apply on col
+# handle col_name not string
         assert not isinstance(col_names, str), "Column names for each table are required!"
         assert len(table_indexes) ==  len(col_names), "Number of tables and number of column sets does not match: {0} != {1}".format(len(table_indexes), len(col_names))
         for table_index, col_name in zip(table_indexes, col_names):
@@ -31,12 +44,15 @@ def single_column_evaluate(tables, func_str, table_indexes, col_names, arg_str=N
     return profile
 
 def main():
-        data1 = sc.parallelize([[1,2],[3,4]])           # => RDD
+        data1 = sc.parallelize([[1,"a"],[3,"c"]])           # => RDD
         data1 = data1.map(lambda x: Row(k1 = x[0], k2 = x[1]))
         table = spark.createDataFrame(data1)
         tables = []
         tables.append(table)
-        print(single_column_evaluate(tables, "describe", 0, ["k1", "k2"], None))
+        tables.append(table)
+        results = single_column_evaluate(tables, "avg", [0, 1], [0, ["k1", "k2"]], None)
+        for result in results:
+            result.show()
 
 if __name__ == "__main__":
     main()
